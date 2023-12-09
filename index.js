6db67a21bd754820f141f73964ce0ae1e50f8a2e
Jrive10 <juliorivera1993@icloud.com>
2023-12-09 16:03:21 -0500

    try

--- index.js
+++ index.js
@@ -13,7 +13,24 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://juliorivera1993:<Jrivera10>@casatrack.2xaldqk.mongodb.net/?retryWrites=true&w=majority', );
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+    startServer();
+  })
+  .catch((err) => {
+    console.error('MongoDB connection error:', err);
+  });
+  
+// Handle unhandled promise rejections (just in case)
+process.on('unhandledRejection', (err) => {
+  console.error('Unhandled Promise Rejection:', err);
+});
+
+// Handle uncaught exceptions (just in case)
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught Exception:', err);
+});
 
 app.use('/projects', projectRoutes);
 app.use('/materials', materialRoutes);
